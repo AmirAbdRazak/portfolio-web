e39a6482d254789666b18f9d1c9e4206db8bd2da
AmirAbdRazak <amrrzk02@gmail.com>
2023-08-05 14:42:20 +0800

    implement get chart list function and add surf crate

--- src/history_fm/weekly_charts.rs
+++ src/history_fm/weekly_charts.rs
@@ -1,5 +1,10 @@
 use async_graphql::{Object, SimpleObject};
-use serde::Serialize;
+use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::env;
+use surf;
+use tracing::info;
 
 // Enum support is pretty eh for async graphql if the enum variants aren't of the same structure, on top of that I couldn't pass generic traits and stuff into types
 // Hence this abonimation is necessary, for now, maybe I'm being dumb about this
@@ -45,6 +50,40 @@ struct TrackEntryChart {
     chart_list: Vec<TrackEntry>,
 }
 
+#[derive(Debug, Deserialize)]
+struct WeeklyChartEntry {
+    #[serde(rename = "#text")]
+    text: String,
+    from: String,
+    to: String,
+    // Add other fields as needed
+}
+
+#[derive(Debug, Deserialize)]
+struct WeeklyChartList {
+    chart: Vec<WeeklyChartEntry>,
+}
+
+#[derive(Debug, Deserialize)]
+struct WeeklyChartListResponse {
+    weeklychartlist: WeeklyChartList,
+}
+
+async fn get_chart_list<'a>(username: &'a str) -> Result<Vec<WeeklyChartEntry>, surf::Error> {
+    info!("Inside get chart list");
+    dotenv().ok();
+    let api_key = env::var("LASTFM_API_KEY").expect("LASTFM_API_KEY is not set");
+    let api_url = String::from(format!("http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user={}&api_key={}&format=json", username, api_key));
+
+    let WeeklyChartList { chart } = surf::get(api_url)
+        .recv_json::<WeeklyChartListResponse>()
+        .await
+        .expect("Error when calling surf API on weeklychartlist")
+        .weeklychartlist;
+
+    Ok(chart)
+}
+
 #[derive(Default)]
 pub struct WeeklyChartsQuery;
 
@@ -55,6 +94,14 @@ impl WeeklyChartsQuery {
         // ctx: &Context<'ctx>,
         // lastfm_username: String,
     ) -> Vec<ArtistEntryChart> {
+        let available_chart_list = get_chart_list("ryzlegg")
+            .await
+            .expect("Error getting chart list");
+
+        for chart in available_chart_list {
+            info!("From: {}, To: {}", chart.from, chart.to);
+        }
+
         let chart_list = vec![ArtistEntry {
             name: String::from("Tutara Peak"),
             playcount: 2540,
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), sqlx::Error> {
     tracing_subscriber::fmt::init();
     dotenv().ok();
 
-    let db_url = env::var("DATABASE_URL").expect("POSTGRES_URL is not set");
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
 
     let pg_pool = PgPoolOptions::new()
         .max_connections(5)
