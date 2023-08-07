160e9e5f81cb2b9a0e6e6de6e7531043de57de94
AmirAbdRazak <amrrzk02@gmail.com>
2023-08-07 22:26:17 +0800

    add migration for user table

--- axum/migrations/0002_add_user.sql
+++ axum/migrations/0002_add_user.sql
@@ -1,7 +1,13 @@
 create table users (
-    id uuid primary key default gen_random_uuid();
-    spotify_id varchar(50) not null,
+    id uuid primary key default gen_random_uuid(),
+    spotify_id varchar(50),
     email varchar(100),
+    image_url varchar(100),
+    registered_unixtime integer,
+    playcount integer,
+    artist_count integer,
+    album_count integer, 
+    track_count integer,
     created_at timestamp not null default now(),
     updated_at timestamp
 );
@@ -13,6 +19,12 @@ create table users_top_artists (
     playcount integer,
     foreign key (user_id) references users (id) on delete cascade,
     foreign key (artist_id) references artists (id) on delete cascade
+);
+
+create table weekly_chart (
+    id uuid primary key default gen_random_uuid(),
+    chart_from integer,
+    chart_to integer
 );
 
 alter table artists
