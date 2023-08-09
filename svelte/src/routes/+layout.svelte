36f917d4897732e1d1d3be79da7857f8222b7592
AmirAbdRazak <amrrzk02@gmail.com>
2023-08-10 00:42:43 +0800

    progress uwu

---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;600;800&display=swap');
+
+        body {
+            font-family: 'Montserrat';
+            font-style: 'normal';
+            font-weight: 400;
+
+        }
+	</style>
+</svelte:head>
+
+<slot />
---
+++ svelte/src/routes/history_fm/charts/+page.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import chart_image from '$lib/assets/chart_image.png';
+	import { request, gql } from 'graphql-request';
+
+	import { useQuery } from '@sveltestack/svelte-query';
+
+	let username: string = 'Enter';
+	let chartData: ArtistChartResponse;
+
+	const endpoint = 'http://localhost:8000/graphql';
+
+	type ArtistEntry = {
+		name: string;
+		playcount: number;
+		attr: {
+			rank: string;
+		};
+	};
+
+	type ArtistChart = {
+		attr: {
+			from: number;
+			to: number;
+		};
+		artist: ArtistEntry[];
+	};
+
+	type ArtistChartResponse = {
+		data: {
+			historyFm: {
+				getWeeklyCharts: {
+					artist: ArtistChart;
+				};
+			};
+		};
+	};
+
+	const document = gql`
+		query {
+			historyFm {
+				getWeeklyCharts {
+					artist(lastfmUsername: $username) {
+						attr {
+							from
+							to
+						}
+						artist {
+							name
+							playcount
+							attr {
+								rank
+							}
+						}
+					}
+				}
+			}
+		}
+	`;
+
+	const getChartData = async () => {
+		const { artist }: { artist: ArtistChartResponse } = await request(endpoint, document);
+		chartData = artist;
+	};
+</script>
+
+<section class="bg-white dark:bg-slate-900 p-10">
+	<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+		<div class="mr-auto place-self-center lg:col-span-7">
+			<h1
+				class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
+			>
+				<!-- svelte-ignore a11y-missing-attribute -->
+				Check out your listening trends for <a class="text-rose-400">LastFM</a> users
+			</h1>
+			<p
+				class="max-w-2xl mb-6 font-light text-slate-700 lg:mb-8 md:text-lg lg:text-xl dark:text-slate-200"
+			>
+				Compare and observe how your music taste evolved over time, tracks you've been (or had been)
+				obsessed with, artists you've been a number one fan of and more!
+			</p>
+			<input
+				class="inline-flex items-center justify-center px-5 py-3 mr-3 text-slate-900 font-medium drop-shadow-lg text-center bg-slate-100 border-2 border-slate-700 rounded-lg bg-primary-700 focus:ring-3 focus:ring-slate-800 focus:outline-none"
+				bind:value={username}
+			/>
+			<button
+				on:click={getChartData}
+				class="inline-flex items-center justify-center px-5 py-3 text-base font-semibold text-center text-slate-900 bg-rose-400 border rounded-lg hover:bg-slate-100 focus:ring-4 focus:ring-slate-100 dark:text-slate-100 dark:border-rose-700 dark:hover:bg-rose-700 dark:focus:ring-slate-800 focus:outline-none"
+			>
+				Generate chart
+			</button>
+		</div>
+		<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+			<img src={chart_image} alt="temp chart" class="object-contain" />
+		</div>
+	</div>
+</section>
+
+{chartData}
