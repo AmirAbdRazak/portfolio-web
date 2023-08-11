a70b308560f5ccadea137c2dde145539cf5a4f32
AmirAbdRazak <amrrzk02@gmail.com>
2023-08-11 18:16:31 +0800

    do some parsing

--- svelte/src/routes/history_fm/charts/[username]/+page.svelte
+++ svelte/src/routes/history_fm/charts/[username]/+page.svelte
@@ -1,7 +1,61 @@
 <script lang="ts">
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
+	import { getContextClient, queryStore } from '@urql/svelte';
+	import { ArtistChartDocument, type ArtistChartQuery } from '../../../../generated/graphql';
+	import { error } from '@sveltejs/kit';
 
 	export let data: PageData;
+	let username = data.username;
+
+	type ArtistPlaycountMapping = {
+		[artist_name: string]: {
+			date: string;
+			playcount: number;
+		}[];
+	};
+
+	let artistPlaycountMapping: ArtistPlaycountMapping;
+
+	$: artistPlaycountMapping = {};
+
+	$: queryData = queryStore({
+		client: getContextClient(),
+		query: ArtistChartDocument,
+		variables: { username }
+	});
+	function generateChart() {
+		const rawArtistData: ArtistChartQuery = $queryData.data;
+		const artistData = rawArtistData.historyFm.getWeeklyCharts.artist;
+
+		const dateRanges = artistData.map((artistEntry) => artistEntry.attr.to);
+		artistData.forEach((artistEntry) => {
+			artistEntry.artist.forEach((artist) => {
+				if (!(artist.name in artistPlaycountMapping)) {
+					artistPlaycountMapping[artist.name] = [];
+				}
+
+				artistPlaycountMapping[artist.name].push({
+					date: artistEntry.attr.to,
+					playcount: parseInt(artist.playcount)
+				});
+			});
+		});
+
+		console.log('===artist play count mapping===');
+		console.log(artistPlaycountMapping);
+
+		console.log(dateRanges);
+
+		const m = artistPlaycountMapping;
+		return m;
+	}
 </script>
 
-<h1>{data}</h1>
+{#if $queryData.fetching}
+	<p>Loading...</p>
+{:else if $queryData.error}
+	<p>{$queryData.error.message}</p>
+{:else}
+	{JSON.stringify(generateChart())}
+	{JSON.stringify(artistPlaycountMapping)}
+{/if}
--- svelte/src/routes/history_fm/charts/[username]/+page.ts
+++ svelte/src/routes/history_fm/charts/[username]/+page.ts
@@ -1,44 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { getContextClient, queryStore } from '@urql/svelte';
-import { ArtistChartDocument, type ArtistChartQuery } from '../../../../generated/graphql';
-import { generate } from '@graphql-codegen/cli';
 
-export const load: PageLoad = async ({ params }) => {
-	let username = params.username;
-
-	let client = getContextClient();
-
-	let queryData = await client.query(ArtistChartDocument, { username });
-
-	function generateChart() {
-		const rawArtistData: ArtistChartQuery = queryData.data;
-		const artistData = rawArtistData.historyFm.getWeeklyCharts.artist;
-
-		const dateRanges = artistData.map((artistEntry) => artistEntry.attr.to);
-
-		console.log(dateRanges);
-	}
-
-	// const data = {
-	// 	labels: labels,
-	// 	datasets: [
-	// 		{
-	// 			label: 'Dataset 1',
-	// 			data: Utils.numbers(NUMBER_CFG),
-	// 			borderColor: Utils.CHART_COLORS.red,
-	// 			backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-	// 			yAxisID: 'y'
-	// 		},
-	// 		{
-	// 			label: 'Dataset 2',
-	// 			data: Utils.numbers(NUMBER_CFG),
-	// 			borderColor: Utils.CHART_COLORS.blue,
-	// 			backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-	// 			yAxisID: 'y1'
-	// 		}
-	// 	]
-	// };
-
-	generateChart();
+export const load: PageLoad = ({ params }) => {
+	const username = params.username;
+	return { username };
 };
