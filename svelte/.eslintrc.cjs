504bba3bf82abf9cc47d8a94213af8a217418fa0
AmirAbdRazak <amrrzk02@gmail.com>
2023-08-30 01:36:36 +0800

    i just spent 1 hour just debugging eslint errors i am done for tonight

--- svelte/.eslintrc.cjs
+++ svelte/.eslintrc.cjs
@@ -1,15 +1,32 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
+		project: 'jsconfig.json',
 		extraFileExtensions: ['.svelte']
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	},
-	parser: '@babel/eslint-parser'
+	}
 };
