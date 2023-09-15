f7d75f391a092c654e13a3cd446cd0d5a1e2f366
AmirAbdRazak <amrrzk02@gmail.com>
2023-09-15 11:56:52 +0800

    dkjfajdlfk

--- svelte/src/routes/charts/+layout.server.ts
+++ svelte/src/routes/charts/+layout.server.ts
@@ -2,6 +2,7 @@ import { env } from '$env/dynamic/private';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async () => {
+	console.log(env.AXUM_URL);
 	return {
 		backend_url: env.AXUM_URL || 'http://localhost:8000/'
 	};
